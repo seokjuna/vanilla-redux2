7058a9a0a42643a985ff8e4a0be93757db30c041
naseokju <lukas95@naver.com>
2023-03-27 14:06:51 +0900

    setup

--- src/index.js
+++ src/index.js
@@ -1,73 +1,8 @@
-import { createStore } from "redux";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./components/app";
 
-const form = document.querySelector("form");
-const input = document.querySelector("input");
-const ul = document.querySelector("ul");
-
-const ADD_TODO = "ADD_TODO";
-const DELETE_TODO = "DELETE_TODO";
-
-const addToDo = (text) => {
-  return {
-    type: ADD_TODO, 
-    text
-  }
-};
-const deleteToDo = (id) => {
-  return {
-    type: DELETE_TODO,
-    id
-  }
-};
-
-const reducer = (state=[], action) => {
-  switch(action.type) {
-    case ADD_TODO:
-      const newToDoObj = { text: action.text, id: Date.now() };
-      return [newToDoObj, ...state];
-    case DELETE_TODO:
-      const cleaned = state.filter(toDo => toDo.id !== action.id);
-      return cleaned;
-    default:
-      return state;
-  }
-};
-
-const store = createStore(reducer);
-store.subscribe(() => console.log(store.getState()));
-
-const dispatchAddTodo = (text) => {
-  store.dispatch(addToDo(text));
-}
-
-const dispatchDeleteTodo = (e) => {
-  const id = parseInt(e.target.parentNode.id);
-  store.dispatch(deleteToDo(id));
-}
-
-const paintToDos = () => {
-  const toDos = store.getState();
-  ul.innerHTML = "";
-  toDos.forEach(toDo => {
-    const li = document.createElement("li");
-    const btn = document.createElement("button");
-    btn.innerText = "DEL";
-    btn.addEventListener("click", dispatchDeleteTodo);
-    li.id = toDo.id
-    li.innerText = toDo.text;
-    ul.appendChild(li);
-    li.appendChild(btn);
-  })
-}
-store.subscribe(paintToDos);
-
-
-
-const onSubmit = e => {
-  e.preventDefault();
-  const toDo = input.value;
-  input.value = "";
-  dispatchAddTodo(toDo);
-};
-
-form.addEventListener("submit", onSubmit);
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <App />
+);
